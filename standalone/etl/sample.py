3e3589768cc03b89099a6335b1a6c733b666ca59
Kendell <contact@kendelljoseph.com>
2012-12-20 12:13:05 -0600

    Added action field

--- standalone/etl/sample.py
+++ standalone/etl/sample.py
@@ -7,8 +7,9 @@ layers = arcpy.mapping.ListLayers(mxd,"",dataFrame)
 def init(layer):
 	cursor = arcpy.SearchCursor(layer)
 	shapefield = arcpy.Describe(layer).ShapeFieldName
-
+	
 	for row in cursor:
+		action 	= row.getValue('action')
 		routeID = row.getValue('RTE_ID')
 		routeGeometry = row.getValue(shapefield).getPart()
 		
@@ -18,9 +19,11 @@ def init(layer):
 			M = [coords[0].M, coords[len(coords)-1].M]
 			
 			arcpy.AddMessage("RouteID: %s" 					% routeID)
+			arcpy.AddMessage("ACTION: %s" 					% action)
 			arcpy.AddMessage("M Start: %s \nM End: %s" 		% (M[0], M[1]))
 			arcpy.AddMessage("X Start: %s \nX End: %s" 		% (X[0], X[1]))
 			arcpy.AddMessage("Y Start: %s \nY End: %s\n" 	% (Y[0], Y[1]))
+			arcpy.AddMessage("Total M: %s \n" 				% math.fabs(M[0]-M[1]))
 
 for layer in layers:
 	roadwayDatasetName = 'sample' # 'TPP_GIS.MCHAMB1.TXDOT_Roadways'
