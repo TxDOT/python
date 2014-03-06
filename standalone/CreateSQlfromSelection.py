89f33ea8b47088372b77d79d03aff2f9249e2703
ABREZNIC <adam.breznicky@txdot.gov>
2014-03-06 16:05:41 -0600

    Description update.

--- standalone/CreateSQlfromSelection.py
+++ standalone/CreateSQlfromSelection.py
@@ -1,4 +1,18 @@
+#March 2014
+#Adam Breznicky, TxDOT TPP - Mapping Group
+#
+#This script is designed to be used as a 'tool' within an ArcGIS Toolbox and run within an MXD.
+#
+# It requires only a single parameter input.
+#When working within an MXD, the parameter required is the 'layer' with a selection which you wish to create a query
+#to identify.
+#
+#The result is a .txt file, saved on the users desktop, and 'popped' open for immediate use.
+#
+#
+
 import arcpy, os
+
 
 def createsqlquery():
     wksp = os.path.expanduser("~\\Desktop\\")
