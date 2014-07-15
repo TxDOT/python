b211830c637406aaea14fef026aa94f35fed6935
UnchartedDrake <davidhickman13@gmail.com>
2014-07-15 10:28:30 -0500

    Formatting changes

--- module/tpp.py
+++ module/tpp.py
@@ -1,3 +1,18 @@
+# -----------------------------------------------------------------------------
+# Author:      Texas Department of Transportation (TPP)
+#
+# Licence:  The MIT License (MIT)
+# Copyright (C) 2014 TxDOT
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# -----------------------------------------------------------------------------
+
 def txArchive():
     """
     Create an archive of your MyModules script and push the working script
